c4706ff4fb1c3b084244c0c66637692bac3990cb
Paulche1001 <thisispaulche@gmail.com>
2023-11-21 00:44:40 +0100

    Weiter an Kopfrechnen gearbeitet

--- lib/pages/kopfrechnen.dart
+++ lib/pages/kopfrechnen.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 class KopfMain extends StatelessWidget {
@@ -13,21 +14,77 @@ class KopfMain extends StatelessWidget {
       ),
       body: GestureDetector(
         behavior: HitTestBehavior.opaque,
-        onTap: () => print("tapped"),
-        child: const KopfInGame(),
+        onTap: (){
+          Navigator.of(context).pushReplacement(
+            MaterialPageRoute(
+                builder: (context) => const KopfInGame(),
+            )
+          );
+
+        },
+        child: DefaultTextStyle.merge(
+          style: const TextStyle(
+            fontSize: 30,
+          ),
+          child: const Center(
+            child: Text("Drücke auf den Bildschirm um zu Starten!"),
+          )
+        ),
       ),
     );
   }
 }
 
-class KopfInGame extends StatelessWidget {
+
+
+class KopfInGame extends StatefulWidget {
   const KopfInGame({super.key});
+  @override
+  State<KopfInGame> createState() => _KopfInGameState();
+}
+
+class _KopfInGameState extends State<KopfInGame> {
+
+  int _counter = 60;
+
+  void _startTimer(){
+    setState(() {
+      if (_counter >= 0) {
+        _counter--;
+      } else {
+        _counter = 0;
+      }
+    });
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    Timer(const Duration(seconds: 1), _startTimer);
+    return Scaffold(
       backgroundColor: Colors.deepPurple,
+      appBar: AppBar(
+        backgroundColor: const  Color.fromRGBO(1, 1, 1, 0.0),
+        elevation: 0.0,
+        automaticallyImplyLeading: false,
+      ),
+      body:  Column(
+        children: [
+          DefaultTextStyle.merge(style:
+            const TextStyle(
+                  fontSize: 35,
+              color: Colors.white,
+            ),
+              child:
+              Center(
+                child: Text("$_counter")
+              )
+          ),
 
+      ],
+      )
     );
   }
 }
+
